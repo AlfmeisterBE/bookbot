f62f6b7982c5f5c8b8b85dfee5f256afdc4021d3
AlfmeisterBE <svenwittevrongel@hotmail.com>
2024-08-04 15:10:02 +0100

    Finished the last step

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ def main():
     num_words = get_num_words(text)
     character_count = get_character_count(text)
     report = get_report(character_count,book_path,num_words)
-    print(f"{num_words} words found in the document")
-    print(character_count)
+    #print(f"{num_words} words found in the document")
+    #print(character_count)
     print(report)
 
 
@@ -29,14 +29,26 @@ def get_character_count(text):
             character_dict[lowered] = 1
     return character_dict
 
+def sort_on(dict):
+    return dict["count"]
+
 def get_report(character_count, book_path,num_words):
     list_dict = []
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document")
-    print("")
-    for k,v in character_count.sort(reverse=True, key=v):
+    report_strings = []
+    report_strings.append(f"--- Begin report of {book_path} ---")
+    report_strings.append(f"{num_words} words found in the document")
+    report_strings.append("")
+
+    for k,v in character_count.items():
         if k.isalpha() == True:
-            print(f"Key is {k} and value is {v}")
+            list_dict.append({"character": k, "count": v})
+    list_dict.sort(reverse=True, key=sort_on )
+
+    for item in list_dict:
+            report_strings.append(f"The '{item['character']}' character was found {item['count']} times")
+    report_strings.append("--- End report ---")
+    
+    return "\n".join(report_strings)
 
 
 if __name__ == "__main__":
